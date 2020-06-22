5abcc6ae2caa7430cc5b87d5f6c4fc6af269e31a
capthapl <dawidkacprzak@icloud.com>
2020-06-22 18:40:46 +0200

    Moved to MVC

--- Oyacode.WEB/wwwroot/js/site.js
+++ Oyacode.WEB/wwwroot/js/site.js
@@ -2,3 +2,39 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
+
+
+let hamburger = document.getElementById('header_menu_mobile');
+let slideMenu = document.getElementById('header_menu_slide');
+let slideX = document.getElementById('header_links_slide_close');
+let mobile_links = document.getElementsByClassName('mobile_link');
+
+window.onload = () => {
+    console.log("load" + mobile_links.length)
+    for (let i = 0; i < mobile_links.length; i++) {
+        mobile_links[i].addEventListener('click', () => {
+            slideMenu.style.padding = "0"
+            slideX.style.width = "0px"
+        })
+    }
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
+}
+
+hamburger.addEventListener('click', () => {
+    slideMenu.style.padding = "20px 250px 10px 30px"
+    slideX.style.width = "30px"
+});
+
+slideX.addEventListener('click', () => {
+    slideMenu.style.padding = "0"
+    slideX.style.width = "0px"
+
+})
